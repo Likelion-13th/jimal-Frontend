f1f600727c67386657d954b66f02ad07e6958cab
qkwltkwkd1 <wldndi2@kau.kr>
2025-03-27 20:19:29 +0900

    250327 frontend

---
+++ src/pages/Mypage/Mypage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Mypage=()=>{
+    return(
+        <div>
+            <h1>얍얍</h1>
+            <p>멋쟁이 사자처럼 13기</p>
+        </div>
+
+    )
+}
+
+export default Mypage;
