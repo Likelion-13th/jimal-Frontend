ee2172ad02cfddf5f9063eabdd3a23fa6354b21a
qkwltkwkd1 <wldndi2@kau.kr>
2025-05-03 22:46:37 +0900

    mypage 주문리스트트

--- src/pages/Mypage/Mypage.js
+++ src/pages/Mypage/Mypage.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import "../../styles/Mypage.css";
+import Profile from './Profile';
+import Status from './Status';
+import Address from './Address';
+import History from './History';
 
 const Mypage=()=>{
     return(
-        <div>
-            <h1>얍얍</h1>
-            <p>멋쟁이 사자처럼 13기</p>
+        <div className="page-container">
+            <Profile />
+            <Status />
+            <Address />
+            <History />
         </div>
 
     )
